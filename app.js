cbbdfa068d8e7c66d858ef3eedd7e2df7b639d20
Ryo <kanadajinlee@yahoo.ca>
2017-03-24 22:22:45 -0500

    Just added express and ejs to package.JSON file, and made sure the server can run properly. Time to get started

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let express = require("express");
+let app = express();
+
+
+
+
+
+
+
+app.listen(3000, function () {
+  console.log('The YelpCamp Server has started (on port 3000)!')
+})
