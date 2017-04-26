a4374d2d6aabdadda2b272dd3162c02609e12936
Ryo <kanadajinlee@yahoo.ca>
2017-04-26 14:52:10 -0500

    Made edit&update routes for comment (just like campgrounds in recent pushes) and the edit form works well (動画#293)

--- routes/comments.js
+++ routes/comments.js
@@ -55,4 +55,27 @@ router.post("/", isLoggedIn, (req, res) => {
    });
 });
 
+
+// Comments Edit
+router.get("/:comment_id/edit", (req, res) => {
+   Comment.findById(req.params.comment_id, (err, foundComment) => {
+      if(err){
+          res.redirect("back");
+      } else {
+        res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+      }
+   });
+});
+
+// Comments Update
+router.put("/:comment_id", (req, res) => {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
+        if(err){
+            res.redirecT("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
 module.exports = router;
